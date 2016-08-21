182324517b561b0128711289573e18b606ad5af4
Noa Fisher <Noa Fisher>
2016-08-21 04:25:13 +0300

    refactoring the code for folders

--- custom-select.js
+++ custom-select.js
@@ -2,80 +2,103 @@
 
 // select function
 
-var select = document.querySelector('select');
-var qframe = document.querySelector('.quick-frame');
-var qexpand = document.querySelector('.quick.expand');
-var form = document.querySelector('.tabs form');
-var settingBtn = document.querySelectorAll('.setting-icon');
-window.onload = addToSelect();
+var select = document.querySelectorAll('select');
+var iframe = document.querySelectorAll('.dynamic-frame');
+var expand = document.querySelectorAll('.dynamic-expand.expand');
+var form = document.querySelectorAll('.tabs form');
+var settingBtns = document.querySelectorAll('.setting-icon');
+window.onload = activateSelect();
 
-function toggleForm(that) {
-    var targetNode;
-    if (this.className === 'setting-icon'){
-        targetNode = this.parentNode.parentNode.nextElementSibling;
+function initSettingBtns(){
+    Array.prototype.forEach.call(settingBtns, function(elem,index,array){
+        elem.addEventListener('click',function(){
+            toggleForm(this,'tabs-containers is-active',goToForm);
+        });
+    });
+}
+function toggleForm(node,parentClass,cb){
+    if(node.className === parentClass ){
+        cb(node);
+        return;
     }
-    else {
-        targetNode = that.parentNode;
-    }
-
-    if(!(targetNode.className === 'custom-frame-box open-box')) {
+    node = node.parentNode;
+    toggleForm(node,parentClass,cb);
+}
+function goToForm(node){
+  var targetNode = node.querySelector('.custom-frame-box');
+    if (!(targetNode.className === 'custom-frame-box open-box')) {
         targetNode.classList.add('open-box');
     }
     else {
         targetNode.classList.remove('open-box');
     }
 }
-settingBtn[0].addEventListener('click',toggleForm);
+initSettingBtns();
 
 
-form.addEventListener('submit', function (e) {
-    e.preventDefault();
-    var newObj = {};
-    var filled = form.elements;
-    for(var i = 1;i <= 3;i++){
-        if(filled['qtitle' + i].value&&filled['qurl' + i].value){
-           newObj[filled['qtitle' + i].value] = filled['qurl' + i].value;
-        }
-    }
-    saveOnLocalstorage(newObj);
-    toggleForm(this);
-
-
-});
-
-function saveOnLocalstorage(obj){
-    localStorage.clear();
-    localStorage.setItem('reports', JSON.stringify(obj));
-    addToSelect();
-}
-function addToSelect(){
-    if(select.length >= 0){
-       for(var j = 0;j < select.length;j++){
-           select.remove(j);
-       }
-    }
-    var newList = JSON.parse(localStorage.getItem('reports'));
-    for(key in newList) {
-        var opt = document.createElement("option");
-        opt.value = newList[key];
-        opt.text = key;
-        select.add(opt, null);
-        displayIframe();
-        //don't forget the _blank
-    }
-
-}
-    function displayIframe(){
-        if(select.selectedIndex > -1){qframe.src = select.options[0].value;}
-    }
-
-
-
-    select.addEventListener('change', function (e) {
-        var newReport = e.target.value;
-        qframe.src = newReport;
-        qexpand.href = newReport;
+function initForms() {
+    Array.prototype.forEach.call(form, function (elem, index, array) {
+        handlerForms(elem);
     });
+}
+
+function handlerForms(link) {
+    link.addEventListener('submit',function(e) {
+        e.preventDefault();
+        var newObj = {};
+        var filled = this.elements;
+        var key = this.id;
+        for (var i = 1; i <= 3; i++) {
+            if (filled['title' + i].value && filled['url' + i].value) {
+                newObj[filled['title' + i].value] = filled['url' + i].value;
+            }
+        }
+        saveOnLocalstorage(newObj,key);
+        toggleForm(this,'custom-frame-box',goToForm);
+    });
+}
+initForms();
+
+
+function saveOnLocalstorage(obj, key) {
+    localStorage.removeItem(key);
+    localStorage.setItem(key, JSON.stringify(obj));
+   activateSelect();
+}
+function activateSelect() {
+    Array.prototype.forEach.call(select, function (el,ind,arr) {
+        el.length = 0;
+        var newList = JSON.parse(localStorage.getItem(el.name));
+        for (var key in newList) {
+            var opt = document.createElement("option");
+            opt.value = newList[key];
+            opt.text = key;
+            el.add(opt, null);
+        }
+    });
+    displayIframe()
+}
+function displayIframe() {
+    Array.prototype.forEach.call(select, function (el,ind,arr) {
+        var firstOption = el.options[0].value;
+        if (el.selectedIndex > -1) {
+            iframe[ind].src = firstOption;
+            expand[ind].href = firstOption;
+        }
+    });
+}
+
+Array.prototype.forEach.call(select, function (el,ind,arr) {
+    onChangeHandler(el,ind)
+});
+function onChangeHandler(el,ind){
+el.addEventListener('change', function (e) {
+    var newReport = e.target.value;
+    iframe[ind].src = newReport;
+    expand[ind].href = newReport;
+});
+}
+
 
 // })();//end
 
