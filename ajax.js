98b80148b910bb3e7b110c074875a0c3e63aff65
Noa Fisher <Noa Fisher>
2016-08-22 21:12:13 +0300

    date for notif

--- ajax.js
+++ ajax.js
@@ -1,12 +1,14 @@
 //UTILS.qs('.notifications').innerHTML = '<span>'+ new Date()+'<span>';
-var notif = UTILS.qs('.notifications')
+var notif = UTILS.qs('.notifications');
+var notifDate = new Date().toDateString()
 var opt = {
     done: function(para){
-         notif.innerHTML = '<span>' + para['notifications'] + '</span>';
+         notif.innerHTML = '<span>' + para['notifications'] + '</span>' + '<span>' + notifDate + '</span>';
     },
     fail: function (para) {
         return console.log(para);
     }
 };
  UTILS.ajax('not.json',opt);
+
 
